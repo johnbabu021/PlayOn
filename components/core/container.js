68ec5b30b12fc54eca0b4d8fbd5dc19de226b226
johnbabu021 <johnbabu021@gmail.com>
2022-01-21 23:32:55 +0530

    feat:style conf added

--- components/core/container.js
+++ components/core/container.js
@@ -1,24 +1,25 @@
 import { useEffect, useState } from 'react'
 import  Songs   from        '../../constants/main.json'
-
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 export  default function    Container(){
 
-    // const   [song,setSong]=useState('')
-    // useEffect(()=>{
-    //     setSong(Songs.json())
-    // },[Songs,song])
-    // console.log(song)
+    const   [song,setSong]=useState('')
+    
     return  (
 <section   
  className="grid 
- gap-4 
- grid-cols-4 
-
+ gap-8 
+ xl:grid-cols-4
+ md:grid-cols-3 
+sm:grid-cols-2
  ">
 {
     Songs?.map((item,index)=>(
-            <div key={index}  className='
-            w-40
+           <div  key={index}   className='grid place-items-center gap-4'>
+                <div   className={
+                    `
+                    
+                    w-40
             h-50
             rounded-full
             h-36
@@ -28,16 +29,41 @@ export  default function    Container(){
             border-8
             border-zinc-200
             shadow-lg
-            shadow-zinc-500
+            shadow-gray-300
             hover:scale-105
             ease-in
+            active:scale-90
+            
             duration-300
             
-            
-            '    >
-                hello
+            ${song===index&&'    active:border-8    border-blue-400'}
+            `
+                }  >
+                <div
+                className='grid place-items-center'
+                >
+                
+                <div    onClick={()=>{setSong(index)}}
+                 className='bg-white 
+                 text-purple-800
+                 rounded-full
+                  w-10 h-10 
+                  grid
+                   place-items-center
+                   shadow-lg
+                   hover:scale-105
+                   ease-in-out
+                   active:scale-90
+                   duration-300
+                   shadow-gray-300
+                   '>
+                <PlayArrowIcon />
+                    </div>
+                </div>
             
             </div>
+            {item.name}
+               </div>
         )
     )
 }
--- components/utils/header.js
+++ components/utils/header.js
@@ -1,8 +1,8 @@
 export  default function    Header(){
     return  (
-        <div    className="flex items-center  h-20 ">
+        <div    className="flex items-center  h-20 sticky top-0 ">
 
-<h1 className='text-2xl text-blue-600'>
+<h1 className='text-2xl text-blue-600 '>
   PlayOn
 </h1>   
         </div>
