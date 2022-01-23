9eb6394e0a5113c908fba2a42e5a4c42de68aa86
johnbabu021 <johnbabu021@gmail.com>
2022-01-23 15:24:31 +0530

    refractor:production server broke

--- components/utils/header.js
+++ components/utils/header.js
@@ -1,7 +1,7 @@
 // import { useContext } from "react"
 // import SongContext, { SongItems } from "../../hooks/context/songContext"
 
-import Image from "next/image";
+
 
 
 
@@ -12,7 +12,7 @@ export  default function    Header(){
         <div    className="flex items-center  h-20 mb-4  shadow-xl shadow-gray-200 backdrop-blur-sm sticky top-0 z-10">
 
 <h1 className='pl-8 text-2xl text-gray-600 w-40 flex justify-between items-center '>
-  <Image src="/assets/images/playon.svg"  width="80px" height="80px"  alt=""/>
+  <img src="/assets/images/playon.svg"  className="w-8 h-8"  alt=""/>
   PlayOn
 </h1>   
         </div>
